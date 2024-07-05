ecde880e1f8e55d84ab9b7823fb42c9ec412089f
abdialimovtemur <abdialimovtemur@gmail.com>
2024-07-05 08:15:15 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// 1. array ichidagi eng katta sonni toping
+
+
+// let array = [4, 5, 3, 5, 2, 5, 27];
+// function largenumber(array) {
+//     let ArraySort = array.sort(function (a, b){
+//         return b - a;
+// });
+// let large = ArraySort[0];
+// return large
+// }
+// console.log(largenumber(array));
+
+
+
+
+
+
+// 2. array ichidagi eng kichik sonni toping 
+
+
+
+// let array = [43, 215, 34, 45, 32, 54, 27];
+// function smallnumber(array) {
+//     let ArraySort = array.sort(function (a, b){
+//         return a - b;
+// });
+// let small = ArraySort[0];
+// return small
+// }
+// console.log(smallnumber(array));
+
+
+
+// 3. Foydalanuvchi son kiritadi va arrey ichidagi foydalanuvchi sonidan kichik bo’lgan sonlarni ekranga chiqaring
+
+
+
+let userNum = prompt("Sonni kiriting");
+let array = [10, 12, 31, 41, 90, 13]
+function smallnumber(userNum){
+    for (let i = 0; i < array.length; i++) {
+        console.log(array)
+        
+    }
+}                       
