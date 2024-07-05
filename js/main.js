aaff2197e5ab0245845cf1ba0b9424b5e7ba5928
abdialimovtemur <abdialimovtemur@gmail.com>
2024-07-05 10:00:00 +0500

    Commit title

--- js/main.js
+++ js/main.js
@@ -36,11 +36,109 @@
 
 
 
-let userNum = prompt("Sonni kiriting");
-let array = [10, 12, 31, 41, 90, 13]
-function smallnumber(userNum){
-    for (let i = 0; i < array.length; i++) {
-        console.log(array)
-        
-    }
-}                       
+// let userNum = prompt("Sonni kiriting");
+// let array = [10, 2, 3, 12, 12, 31, 41, 90, 13];
+// function smallnumber(userNum){
+//     for (let i = 0; i < array.length; i++) {
+//         if (userNum > array[i]) {
+//             console.log(array[i]);
+//         }
+
+//     }
+// }                       
+
+// smallnumber(userNum,array)
+
+
+
+
+
+// 4. argument sifatida qabul qilingan sonning mukammal yoki mukammal son emasligini aniqlaydigan function yarating 
+
+
+
+
+// let userNum = +prompt("sonni kiriting");
+// let total = 0;
+
+// function perfectNum(userNum){
+//     for(let i = 1; i < userNum; i++)
+//     if(userNum % i == 0){
+//         total = total + i
+//     }if(userNum == total){
+//         console.log("Siz kiritgan son mukammal son")
+//     }else{
+//         console.log("Kiritgan soningiz mukammal emas")
+//     }
+// }
+
+// perfectNum(userNum)
+
+
+
+
+
+// 5. raqamlar kiritilgan arreyni teskari qilish (for dan foydalaning) 
+
+
+// let arr = [23, 21, 5, 3, 52, 22, 14, 2, 54, 24];
+// let arr2 = [];
+
+// function reverseArr(arr){
+//     for(let i = arr.length - 1; i > -1; i-- ){
+//         arr2.push(arr[i]);
+//     }
+//     console.log(arr2)
+// }
+
+// reverseArr(arr,arr2)
+
+
+
+// 6. arrey ichidagi tub sonlar ni toping
+
+
+
+// let userNum = prompt("Sonni kiriting");
+
+// function primeNumber(userNum) {
+//     let total = 0;
+
+//     for (let i = 1; i < userNum; i++) {
+//         if (userNum % i == 0) {
+//             total = total + 1
+//         }
+
+//     } if (total < 2) {
+//         console.log("bu son tub son");
+//     } else {
+//         console.log("bu son tub son emas")
+//     }
+
+// }
+
+// primeNumber(userNum)
+
+
+// 7. Ixtiyoriy function va value lari sonlardan iborat bo'lgan object yaratilsin, va object value laridan tashkil topgan arrayga function parametri ham oxiridan ham boshidan qo'shilsin
+
+
+let values = {
+    firstName: "Temur",
+    lastName: "Abdialimov",
+    age: 20,
+    country: "Uzbekistan",
+};
+
+function exampleFunction(param) {
+    return param;
+}
+
+let valueArray = Object.values(values);
+
+let param = exampleFunction(20);
+valueArray.unshift(param);
+
+valueArray.push(param);
+
+console.log(valueArray);
